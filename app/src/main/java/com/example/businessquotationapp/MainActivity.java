292a6786352c4820eb20fa5a45f2f9e98bb640b6
Marc Plarisan <plarisanmarc2@gmail.com>
2025-03-01 14:04:33 +0800

    Main Activity and card quotation

--- app/src/main/java/com/example/businessquotationapp/MainActivity.java
+++ app/src/main/java/com/example/businessquotationapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.businessquotationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.businessquotationapp.helpers.Utils;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button manage, add;
+    private Spinner category;
+    private SearchView search;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        try{
+            manage.findViewById(R.id.manageBtn);
+            add.findViewById(R.id.addBtn);
+            category.findViewById(R.id.categorySpn);
+            search.findViewById(R.id.searchtxt);
+
+            setSpinner();
+            setButtons();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Utils.longToast(e.getMessage(), this);
+        }
+    }
+
+    public void setButtons(){
+        manage.setOnClickListener(v -> {
+
+        });
+        add.setOnClickListener(v -> {
+
+        });
+    }
+
+    public void setSpinner(){
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.status_array,
+                android.R.layout.simple_spinner_item
+        );
+// Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner.
+        category.setAdapter(adapter);
     }
 }
